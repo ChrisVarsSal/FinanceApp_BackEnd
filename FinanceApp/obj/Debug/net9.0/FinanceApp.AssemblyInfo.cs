86f1b5fd50787faf9b15ae16ce87dffa6ebf5d52
ChrisVarsSal <68918398+ChrisVarsSal@users.noreply.github.com>
2025-09-03 20:04:12 -0600

    Package
    Include some of the packages needed

--- FinanceApp/obj/Debug/net9.0/FinanceApp.AssemblyInfo.cs
+++ FinanceApp/obj/Debug/net9.0/FinanceApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinanceApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7300da5f10206fd59444d09a4f2689cd8940eb80")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53d8e85494feae78219163c427255ccc408480d9")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinanceApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinanceApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
