f2243a672e2c7f27224e1559e7bb38e8b7c8c8a1
ChrisVarsSal <68918398+ChrisVarsSal@users.noreply.github.com>
2025-09-08 11:16:02 -0600

    Updates

--- FinanceApp/obj/Debug/net9.0/FinanceApp.AssemblyInfo.cs
+++ FinanceApp/obj/Debug/net9.0/FinanceApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinanceApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86f1b5fd50787faf9b15ae16ce87dffa6ebf5d52")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b9100d278fbb4b14ef23cbd0eb8905791d903e9")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinanceApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinanceApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
