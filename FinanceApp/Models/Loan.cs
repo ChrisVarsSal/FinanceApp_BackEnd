963fa54eb8180725af4dd8be7ff0687c6eb852ed
ChrisVarsSal <68918398+ChrisVarsSal@users.noreply.github.com>
2025-09-08 11:16:32 -0600

    Create Loan.cs
    Create all the related to the Loan Table

---
+++ FinanceApp/Models/Loan.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinanceApp.Models
+{
+    public class Loan
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public int Amount { get; set; }
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal InterestRate { get; set; }
+        [Column(TypeName = "decimal(18,2)")]
+        public string Status { get; set; } = string.Empty;
+        public decimal MonthlyDeposit { get; set; }
+        public DateTime RequestedDate { get; set; } = DateTime.Now;
+        public int? UserId { get; set; }
+        public User? User { get; set; }
+    }
+}
