c931168ad1d72184fb99adb3f8e48a87a91cbd88
ChrisVarsSal <68918398+ChrisVarsSal@users.noreply.github.com>
2025-09-08 13:05:05 -0600

    User DTOs
    Creation of the DTOs needed for the user

---
+++ FinanceApp/DTOs/Savings/SavingsDto.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinanceApp.DTOs.Savings
+{
+    public class SavingsDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public int Amount { get; set; }
+        public decimal InterestRate { get; set; }
+        public decimal InitialDeposit { get; set; }
+        public decimal MonthlyDeposit { get; set; }
+        public DateTime RequestedDate { get; set; } = DateTime.Now;
+        public int? UserId { get; set; }
+    }
+}
---
+++ FinanceApp/DTOs/User/UserDto.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinanceApp.DTOs.User
+{
+    public class UserDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public string ConfirmPassword { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
+        public string PhoneNumber { get; set; } = string.Empty;
+    }
+}
