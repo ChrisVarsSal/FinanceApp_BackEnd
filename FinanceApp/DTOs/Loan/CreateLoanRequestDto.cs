2a7fca9ba2b05f8b962d2dbef827bdd7b9784822
ChrisVarsSal <68918398+ChrisVarsSal@users.noreply.github.com>
2025-09-08 13:11:51 -0600

    DTOs
    Loan and Savings DTOs

---
+++ FinanceApp/DTOs/Loan/CreateLoanRequestDto.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinanceApp.DTOs.Loan
+{
+    public class CreateLoanRequestDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public int Amount { get; set; }
+        public decimal InterestRate { get; set; }
+        public string Status { get; set; } = string.Empty;
+        public decimal MonthlyDeposit { get; set; }
+        public DateTime RequestedDate { get; set; } = DateTime.Now;
+    }
+}
